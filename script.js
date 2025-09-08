1ebca62199ec60bbda3c3441fb1a9a30c420fedb
VT DESIGN <awev83172@gmail.com>
2025-09-08 21:35:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ const navLinks = document.querySelector('.nav-links');
 menuToggle.addEventListener('click', () => {
   navLinks.classList.toggle('show');
 });
+
+// Toggle hamburger menu
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
