c0ad2a434aab2a7f502c129b7af73013f2335d2e
VT DESIGN <awev83172@gmail.com>
2025-09-08 22:33:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-const menuToggle = document.getElementById("menu-toggle");
+const menuToggle = document.querySelector(".menu-toggle");
 const navLinks = document.querySelector(".nav-links");
 
 menuToggle.addEventListener("click", () => {
   navLinks.classList.toggle("active");
+
+  // Change hamburger ☰ to ✖
+  if (menuToggle.innerHTML === "&#9776;") {
+    menuToggle.innerHTML = "&#10006;"; // X icon
+  } else {
+    menuToggle.innerHTML = "&#9776;"; // Hamburger
+  }
 });
